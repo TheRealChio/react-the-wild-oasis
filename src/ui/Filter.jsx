bd48f18ea6d367751c995e726ea474e183c2631c
Daniel B <d.berneisch@gmx.de>
2023-10-08 18:38:51 +0200

    added filter feature for cabins table

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const StyledFilter = styled.div`
@@ -33,3 +34,30 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currenFilter = searchParams.get(filterField) || options[0].value;
+
+  function handleCLick(value) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleCLick(option.value)}
+          active={option.value === currenFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+export default Filter;
